c13b8b7010311eaf1b43d340127a193ab6cc9424
Kalinin Alexander <kalina783@gmail.com>
2023-06-11 15:11:13 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+
+        List<Employee> list = parseXML("data.xml"); //список сотрудников из xml
+        String json = listToJson(list);
+        writeString(json);
+    }
+
+    public static List<Employee> parseXML(String fileName)
+            throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> list = new ArrayList<>();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+
+        Node staff = doc.getDocumentElement();
+
+        NodeList nodeList = staff.getChildNodes();
+
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element element = (Element) node;
+                int id = Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent());
+                String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                String country = element.getElementsByTagName("country").item(0).getTextContent();
+                int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                Employee employee = new Employee(id, firstName, lastName, country, age);
+                list.add(employee);
+            }
+        }
+        return list;
+    }
+
+    public static <T> String listToJson(List<T> list) {
+        Type listType = new TypeToken<List<T>>() {
+        }.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+
+    public static void writeString(String json) {
+        try (FileWriter file = new FileWriter("data.json");) {
+            file.write(json);
+            file.flush();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}
